1d0e8426eafcaa1ce2369ffa8fdca75c0b1aefe8
chriscampbellt <christopher.campbell.t@gmail.com>
2025-01-10 20:33:58 +0500

    initial commit

---
+++ scripts/celery.py
@@ -0,0 +1,10 @@
+from subprocess import check_call
+
+
+def run_worker():
+    check_call(["celery", "-A", "conf", "worker", "--loglevel=info"])
+    # os.system("celery -A conf worker --loglevel=info")
+
+
+def run_beat():
+    check_call(["celery", "-A", "conf", "beat", "--loglevel=info"])
